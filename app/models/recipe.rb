9d51d9962d6202cdf315a72e1083ea0c6e482dad
Ramon Lima <ramonlima08@gmail.com>
2021-10-09 12:33:32 -0300

    Criando filtros customizados no modelo

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,18 @@ class Recipe < ApplicationRecord
     def light?
         calories < 130
     end
+
+    def self.created_at 
+        where('created_at <= ?', Time.now).order('created_at DESC')
+    end
+
+    # formas de fazer consultas no bd
+    # Recipe.all
+    # Recipe.count
+    # Recipe.order('nome_do_campo DESC')
+    # Recipe.find(1)
+    # Recipe.find_by(name: 'teste')
+    # Recipe.where('campo <= ?', valor)
+    # Recipe.where('created_at <= ?', Time.now).order('created_at DESC')
+
 end
